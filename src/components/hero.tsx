009d18b977735bd7c27aae3bcf9ae5e3972228b7
Codyl <codylillyw@gmail.com>
2026-05-29 19:53:50 -0600

    fix: switch to use VITE_API_URL

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -45,7 +45,7 @@ const Hero = () => {
       return;
     }
 
-    const res = await fetch("http://localhost:4321/api/search", {
+    const res = await fetch(import.meta.env.VITE_API_URL + "/api/search", {
       method: "POST",
       body: JSON.stringify({
         inputText: query,
